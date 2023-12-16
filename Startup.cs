81df2a504efba47237142aaab664dc8946fb23f5
Oana Istoc <o.istoc@outlook.com>
2023-12-16 22:06:59 +0200

    test

---
+++ Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using PetAdopt.Data;
+using Microsoft.AspNetCore.Identity;
+
+
+
+namespace PetAdopt
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PetAdoptContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("PetAdoptContext")));
+
+            services.AddDbContext<LibraryIdentityContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("LibraryIdentityContext")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<LibraryIdentityContext>();
+
+            services.AddRazorPages();
+        }
+    }
+
+
+}
